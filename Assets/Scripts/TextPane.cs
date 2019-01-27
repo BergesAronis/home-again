ff833d17c486fa230471cd2138aecf0cc2563d5b
chapeau-42 <Chapeau-42@users.noreply.github.com>
2019-01-26 20:00:49 -0500

    add interactable object and text pane prefabs

---
+++ Assets/Scripts/TextPane.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPane : MonoBehaviour
+{
+    private float startTime;
+    private bool canContinue;
+
+    private float continueDelay = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.ClosePane();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!this.canContinue && Time.time > this.startTime + this.continueDelay)
+        {
+            this.canContinue = true;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        // TODO: add ability to cycle through multiple dialogue panes (instead of just 1) before closing
+        if (this.canContinue)
+        {
+            this.ClosePane();
+        }
+    }
+
+    public void OpenPane()
+    {
+        // TODO: add ability to pass text through for display
+        this.startTime = Time.time;
+        this.canContinue = false;
+        this.gameObject.SetActive(true);
+    }
+
+    public void ClosePane()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
